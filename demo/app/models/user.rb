adf150066856e48668563ab9ea0e31194a631049
alexsoble <asoble@gmail.com>
2015-02-16 11:34:30 -0500

    Add to README: where to put encryption key.
    Also fix typo.

--- demo/app/models/user.rb
+++ demo/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :two_factor_authenticatable,
          :otp_secret_encryption_key => 'YOUR_ENCRYPTION_KEY_HERE'  # Set a unique encryption key for your app here.
                                                                    # Store your key as an ENV variable and
-                                                                   # remember to add add it to .gitignore 
+                                                                   # remember to add it to .gitignore 
                                                                    # if you plan to share your code publicly.
 
   devise :registerable,
