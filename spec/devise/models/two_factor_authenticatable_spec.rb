8dd11d81c67d77c9a4cb06278d7125d465211062
connorshea <connor.james.shea@gmail.com>
2016-03-09 10:33:16 -0700

    Allow Devise 4.x versions
    Allow Devise versions above 3.5.0, update the README to include
    `devise_perameter_sanitizer` syntax for Devise 4.

--- spec/devise/models/two_factor_authenticatable_spec.rb
+++ spec/devise/models/two_factor_authenticatable_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 require 'active_model'
 
 class TwoFactorAuthenticatableDouble
+  extend ::ActiveModel::Callbacks
+  extend AttrEncrypted
   include ::ActiveModel::Validations::Callbacks
   extend  ::Devise::Models
+
+  define_model_callbacks :update
 
   devise :two_factor_authenticatable, :otp_secret_encryption_key => 'test-key'
 
