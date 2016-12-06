28033e6c47b0106b348679b22e4fb2bee278a944
Diego Rossini Vieira <diego.rossini@plataformatec.com.br>
2016-12-06 13:03:26 -0200

    Update demo application and make it simpler
    - Bump Rails version
    - Remove `pg` gem in favor of `sqlite3` gem
    - Fix devise strong parameters config
    - Update schema.rb
    - Organize `User` model
    - Remove unused gems
    - Add flash messages to layout

--- demo/app/models/user.rb
+++ demo/app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   devise :two_factor_authenticatable,
-         :otp_secret_encryption_key => ENV['ENCRYPTION_KEY']  # Set a unique encryption key for your app.
-                                                                        # Store your key as an ENV variable and
-                                                                        # remember to add it to .gitignore 
-                                                                        # if you plan to share your code publicly.
+         # Set a unique encryption key for your app.
+         # Store your key as an ENV variable and
+         # remember to add it to .gitignore
+         # if you plan to share your code publicly.
+         otp_secret_encryption_key: ENV['ENCRYPTION_KEY']
 
-  devise :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable, :recoverable, :rememberable,
+         :trackable, :validatable
 end
